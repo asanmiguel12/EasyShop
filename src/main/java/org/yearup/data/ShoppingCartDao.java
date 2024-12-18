61905ccedd401d1b19019ac73b43e17f04c33e55
Adrian San Miguel <adrianchristiansanmiguel@gmail.com>
2024-12-18 07:31:58 -0800

    Category Controller

--- src/main/java/org/yearup/data/ShoppingCartDao.java
+++ src/main/java/org/yearup/data/ShoppingCartDao.java
@@ -1,9 +1,14 @@
 package org.yearup.data;
 
+import org.yearup.models.Product;
 import org.yearup.models.ShoppingCart;
+import org.yearup.models.ShoppingCartItem;
 
 public interface ShoppingCartDao
 {
     ShoppingCart getByUserId(int userId);
     // add additional method signatures here
+
+    ShoppingCart getByProductId(int ProductId);
+
 }
